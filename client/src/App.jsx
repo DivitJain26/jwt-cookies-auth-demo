08685f5157b42428b109508e13a83fb502ad53ca
Divit Jain <divit26indore@gmail.com>
2025-05-10 22:58:24 +0530

    Initial commit on new branch

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = (props) => {
+  return (
+    <>
+        <h1 className="text-green-600 text-4xl">Welcome to the Get Health Project</h1>
+    </>
+  )
+};
+
+export default App;
